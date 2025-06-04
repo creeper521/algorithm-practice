f73763850c5171cd6d4c18ad0060a9147368648d
crjs-hao <19603710129@163.com>
2025-06-04 19:51:18 +0800

    算法

--- src/hot100/t3.java
+++ src/hot100/t3.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class t3 {
+    //抛开时间复杂度的话我想到的是滑动窗口
+    //左开头找
     public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
         for(int num : nums){
@@ -23,4 +25,32 @@ public class t3 {
         }
         return nowAns;
     }
+    //左右开找，并找到一个删掉一个
+    public int longestConsecutive2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums) set.add(num);
+        int max = 0;
+        for(int num: nums) {
+            int cur_max = 0;
+            if(set.contains(num)) {
+                int temp = num;
+                while(set.contains(temp)) {
+                    set.remove(temp);
+                    temp++;
+                    cur_max++;
+                }
+
+                temp = num - 1;
+                while(set.contains(temp)) {
+                    set.remove(temp);
+                    temp--;
+                    cur_max++;
+                }
+            }
+
+            max = Math.max(max, cur_max);
+        }
+        return max;
+    }
+
 }
