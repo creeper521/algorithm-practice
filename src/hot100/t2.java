7c3f98595b6c06d31f9dc2c96107018627812f1a
crjs-hao <19603710129@163.com>
2025-06-04 19:08:06 +0800

    算法

--- src/hot100/t2.java
+++ src/hot100/t2.java
@@ -1,11 +1,18 @@
 package hot100;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class t2 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str:strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);//区分toString和new String
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
     }
 }
