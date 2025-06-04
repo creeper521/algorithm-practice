bc1161c455c4bf12febfecb1d72352da7603b191
crjs-hao <19603710129@163.com>
2025-06-04 15:22:42 +0800

    算法

--- src/hot100/t1.java
+++ src/hot100/t1.java
@@ -1,10 +1,13 @@
 package hot100;
 
-import jdk.swing.interop.SwingInterOpUtils;
+//https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class t1 {
 
-    static public int[] twoSum(int[] nums, int target) {
+    static public int[] twoSum1(int[] nums, int target) {
         int ans[] = new int[2];
         for(int i = 0;i < nums.length;i++){
             for(int j = i + 1;j < nums.length;j++){
@@ -15,6 +18,17 @@ public class t1 {
             }
         }
         return ans;
+    }
+
+    static public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0;i < nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
     }
 
     public static void main(String[] args) {
---
+++ src/hot100/t2.java
@@ -0,0 +1,11 @@
+package hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class t2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+
+    }
+}
