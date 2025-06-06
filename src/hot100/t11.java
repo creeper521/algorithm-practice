17563bafc25d2232032774afce8813496457da73
crjs-hao <19603710129@163.com>
2025-06-06 17:32:19 +0800

    算法

--- src/hot100/t11.java
+++ src/hot100/t11.java
@@ -1,28 +1,53 @@
 package hot100;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class t11 {
-    //大根堆
+    //大根堆处理
+//    public int[] maxSlidingWindow(int[] nums, int k) {
+//        int n = nums.length;
+//        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+//            }
+//        });
+//        for(int i = 0;i < k;i++){
+//            queue.offer(new int[]{nums[i], i});
+//        }
+//        int[] ans = new int[n - k + 1];
+//        ans[0] = queue.peek()[0];
+//        for(int i = k;i < n;i++){
+//            queue.offer(new int[]{nums[i], i});
+//            while(queue.peek()[1] <= i - k){
+//                queue.poll();//检查队首元素是否还在当前范围内
+//            }
+//            ans[i - k + 1] = queue.peek()[0];
+//        }
+//        return ans;
+//    }
+
+    //双端队列处理
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] pair1, int[] pair2) {
-                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for(int i = 0;i < k;i++){
+            while(!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
+                deque.pollLast();
             }
-        });
-        for (int i = 0; i < k; ++i) {
-            pq.offer(new int[]{nums[i], i});
+            deque.offerLast(i);
         }
         int[] ans = new int[n - k + 1];
-        ans[0] = pq.peek()[0];
-        for (int i = k; i < n; ++i) {
-            pq.offer(new int[]{nums[i], i});
-            while (pq.peek()[1] <= i - k) {
-                pq.poll();
+        ans[0] = nums[deque.peekFirst()];
+        for(int i = k;i < n;i++){
+            while(!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
+                deque.pollLast();
             }
-            ans[i - k + 1] = pq.peek()[0];
+            deque.offerLast(i);
+            while(deque.peekFirst() <= i - k){
+                deque.pollFirst();
+            }
+            ans[i - k + 1] = nums[deque.peekFirst()];
         }
         return ans;
     }
