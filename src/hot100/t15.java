35d36a74b3d06fa365ccf1b310fc530e0c500ed7
crjs-hao <19603710129@163.com>
2025-06-07 15:20:15 +0800

    算法

---
+++ src/hot100/t15.java
@@ -0,0 +1,20 @@
+package hot100;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class t15 {
+    //额外数组
+//    public void rotate(int[] nums, int k) {
+//        int n = nums.length;
+//        int[] newArr = new int[n];
+//        for (int i = 0; i < n; ++i) {
+//            newArr[(i + k) % n] = nums[i];
+//        }
+//        System.arraycopy(newArr, 0, nums, 0, n);
+//    }
+
+    //临时变量储存旋转值
+
+
+}
