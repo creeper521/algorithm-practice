22ebaf6de002b4dfa421f8d6d17d8c9f338bbf79
crjs-hao <19603710129@163.com>
2025-06-08 11:32:51 +0800

    算法

---
+++ src/hot100/t21.java
@@ -0,0 +1,14 @@
+package hot100;
+
+public class t21 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                if (element == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
