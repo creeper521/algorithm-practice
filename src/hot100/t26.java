58dcfe6c372ba67817389866c50465bc3c10af95
crjs-hao <19603710129@163.com>
2025-06-09 15:23:53 +0800

    算法

--- src/hot100/t26.java
+++ src/hot100/t26.java
@@ -26,24 +26,20 @@ public class t26 {
 //    }
     //先确定是否存在环，再寻找开始节点（下午再来一遍）
     public ListNode detectCycle(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        ListNode slow = head, fast = head;
-        while (fast != null) {
+        if(head == null)return null;
+        ListNode fast = head, slow = head;
+        while(fast != null){
             slow = slow.next;
-            if (fast.next != null) {
+            if(fast.next != null){
                 fast = fast.next.next;
-            } else {
-                return null;
-            }
-            if (fast == slow) {
-                ListNode ptr = head;
-                while (ptr != slow) {
-                    ptr = ptr.next;
+            }else return null;
+            if(fast == slow){
+                ListNode cur = head;
+                while(cur != slow){
+                    cur = cur.next;
                     slow = slow.next;
                 }
-                return ptr;
+                return cur;
             }
         }
         return null;
