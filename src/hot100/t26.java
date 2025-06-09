38bbf7f94d01b4662a0b9c6c4bc3badb5ce0196a
crjs-hao <19603710129@163.com>
2025-06-09 15:53:56 +0800

    算法

--- src/hot100/t26.java
+++ src/hot100/t26.java
@@ -1,13 +1,13 @@
 package hot100;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-      val = x;
-      next = null;
-    }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//    ListNode(int x) {
+//      val = x;
+//      next = null;
+//    }
+//}
 
 public class t26 {
     //哈希表改造
@@ -25,23 +25,23 @@ public class t26 {
 //        return null;
 //    }
     //先确定是否存在环，再寻找开始节点（下午再来一遍）
-    public ListNode detectCycle(ListNode head) {
-        if(head == null)return null;
-        ListNode fast = head, slow = head;
-        while(fast != null){
-            slow = slow.next;
-            if(fast.next != null){
-                fast = fast.next.next;
-            }else return null;
-            if(fast == slow){
-                ListNode cur = head;
-                while(cur != slow){
-                    cur = cur.next;
-                    slow = slow.next;
-                }
-                return cur;
-            }
-        }
-        return null;
-    }
+//    public ListNode detectCycle(ListNode head) {
+//        if(head == null)return null;
+//        ListNode fast = head, slow = head;
+//        while(fast != null){
+//            slow = slow.next;
+//            if(fast.next != null){
+//                fast = fast.next.next;
+//            }else return null;
+//            if(fast == slow){
+//                ListNode cur = head;
+//                while(cur != slow){
+//                    cur = cur.next;
+//                    slow = slow.next;
+//                }
+//                return cur;
+//            }
+//        }
+//        return null;
+//    }
 }
