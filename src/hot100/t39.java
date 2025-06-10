afd6af63dd601395d66fcc735cac1e7224f18934
crjs-hao <19603710129@163.com>
2025-06-10 16:04:20 +0800

    算法

--- src/hot100/t39.java
+++ src/hot100/t39.java
@@ -14,4 +14,12 @@ class TreeNode {
 }
 
 public class t39 {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+    public boolean check(TreeNode r, TreeNode l){
+        if(r == null && l == null)return true;
+        if(r == null || l == null)return false;
+        return r.val == l.val && check(r.left, l.right) && check(r.right, l.left);
+    }
 }
