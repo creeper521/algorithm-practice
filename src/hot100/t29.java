6a18aba4e26f541077144d3d7278e1a40df957cb
crjs-hao <19603710129@163.com>
2025-09-05 16:09:21 +0800

    up

--- src/hot100/t29.java
+++ src/hot100/t29.java
@@ -1,12 +1,12 @@
 package hot100;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//    ListNode() {}
+//    ListNode(int val) { this.val = val; }
+//    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
 
 public class t29 {
 //    public ListNode removeNthFromEnd(ListNode head, int n) {
